7d8aea62c96986cd5d90fe7e03145eccfe552768
mouzmoukhtar <mouzmoukhtar1996@gmail.com.com>
2023-02-20 22:02:24 +0200

    project1

---
+++ project 1/terraform(iac)/Backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "gcs" {
+    bucket = "project1-moaz"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
